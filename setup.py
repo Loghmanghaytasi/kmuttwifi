07a2845666b83793730962cfe1c48b1b471cb513
semicomma@gmail.com <alpha@Aethereal.local>
2009-09-19 14:37:58 +0700

    Prepare for py2app support.

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-from distutils.core import setup
-import py2exe
+import os
 
-setup(
-    windows=['wifi.py'],
-    options=dict(py2exe=dict(includes=['sip']))
-)
+if os.name == 'nt':
+    from distutils.core import setup
+    import py2exe
+    setup(
+        windows=['wifi.py'],
+        options=dict(py2exe=dict(includes=['sip']))
+    )
+
+elif os.name == 'posix':
+    from setuptools import setup
+    setup(
+        app=['wifi.py'],
+        setup_requires=['py2app']
+    )
